9715a4b337998c9ed89950696ebeedfbb7e58199
Philip Agyekum <Arnaudtako970@gmail.com>
2024-02-27 22:44:26 +0530

    Added: Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HttpClientModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HttpClientModule , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
